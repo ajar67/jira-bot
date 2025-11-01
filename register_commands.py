288bb337aa5648357a4c6a94de5d2e24c6753b09
Anthony <anthonyjaranieri@gmail.com>
2025-11-01 12:03:01 -0400

    fix: defer interaction to prevent timeout

--- register_commands.py
+++ register_commands.py
@@ -1,18 +1,18 @@
-import os, requests # type: ignore
-from dotenv import load_dotenv
-load_dotenv()
+# import os, requests # type: ignore
+# from dotenv import load_dotenv
+# load_dotenv()
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-APP_ID = os.getenv("APPLICATION_ID")
-GUILD_ID = os.getenv("GUILD_ID")
+# TOKEN = os.getenv("DISCORD_TOKEN")
+# APP_ID = os.getenv("APPLICATION_ID")
+# GUILD_ID = os.getenv("GUILD_ID")
 
-url = f"https://discord.com/api/v10/applications/{APP_ID}/guilds/{GUILD_ID}/commands"
+# url = f"https://discord.com/api/v10/applications/{APP_ID}/guilds/{GUILD_ID}/commands"
 
-command = {
-    "name": "Later",
-    "type": 3,
-}
+# command = {
+#     "name": "Later",
+#     "type": 3,
+# }
 
-headers = {"Authorization": f"Bot {TOKEN}"}
-r = requests.post(url, headers=headers, json=command)
-print(r.status_code, r.text)
+# headers = {"Authorization": f"Bot {TOKEN}"}
+# r = requests.post(url, headers=headers, json=command)
+# print(r.status_code, r.text)
